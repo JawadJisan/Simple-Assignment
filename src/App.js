447beada5ff547aa5edbd40bf6e7ff661c2c4cdc
zahidmiran <zahidmiron599@gmail.com>
2022-06-06 07:15:33 +0600

    Git Repo added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from 'react';
+import { Button, Card, Col, Row } from 'react-bootstrap';
+
+
+
+
 
 function App() {
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    fetch('http://refertest.pythonanywhere.com/job/openings')
+      .then(res => res.json())
+      .then(data => {
+        setData(data)
+        // console.log(data?.length)
+        console.log(data.data)
+      })
+  }, [])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="">
+      <h1 className='text-center'>List of Opening Jobs : {data.data?.length} </h1>
+      <div>
+        {
+          data?.data?.map((item) =>
+            // <p>{item?.company} </p>
+            <Card style={{ width: '18rem' }}>
+            <Card.Img variant="top" src="holder.js/100px180" />
+            <Card.Body>
+              <Card.Title>{item?.designation} </Card.Title>
+              <Card.Text>
+                {item?.company}
+              </Card.Text>
+              <Card.Text>
+                {item?.id}
+              </Card.Text>
+              <Card.Text>
+                {item?.location}
+              </Card.Text>
+              <Card.Text>
+                {item?.min_experience == 0 ? 'Fresher Opening' : item?.min_experience }
+              </Card.Text>
+              <Button variant="primary">Go somewhere</Button>
+            </Card.Body>
+          </Card>
+          )
+        }
+
+
+      </div>
     </div>
   );
 }
